b02f95646be0cf7af0cd4f71e3945a58a9e688d1
Anssi Juvonen <anssi.juvonen@gofore.com>
2023-02-13 09:51:15 +0200

    Parameterize test

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -1,41 +1,23 @@
-/// <reference types="cypress"/>
-
-const testInitialNavigation = (buttonName: string, location: string) => {
-  cy.getByTestId('version-selection').contains(buttonName).click();
-  cy.location('pathname').should('contain', `${location}/play`);
-  cy.getByTestId('information-modal').getByTestId('continue-button').click();
-};
-
-const testStatsNavigation = () => {
-  cy.getByTestId('stats-link').click();
-  cy.location('pathname').should('contain', 'stats');
-  cy.go('back');
-};
-
 describe('Navigation', () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
-  it('to recoil works', () => {
-    testInitialNavigation('Recoil', 'recoil')
-    testStatsNavigation();
-    cy.go('back');
-    cy.location('pathname').should('not.contain', 'recoil');
-  });
-
-  it('to redux works', () => {
-    testInitialNavigation('Redux', 'redux')
-    testStatsNavigation();
-    cy.go('back');
-    cy.location('pathname').should('not.contain', 'redux');
-  });
-
-  it('to context-api works', () => {
-    testInitialNavigation('Context API', 'context')
-    testStatsNavigation();
-    cy.go('back');
-    cy.location('pathname').should('not.contain', 'context');
+  [
+    {buttonName: 'Recoil', location: 'recoil'},
+    {buttonName: 'Redux', location: 'redux'},
+    {buttonName: 'Context API', location: 'context'},
+  ].forEach(({buttonName, location}) => {
+    it(`with ${buttonName} works`, () => {
+      cy.getByTestId('version-selection').contains(buttonName).click();
+      cy.location('pathname').should('contain', `${location}/play`);
+      cy.getByTestId('information-modal').getByTestId('continue-button').click();
+      cy.getByTestId('stats-link').click();
+      cy.location('pathname').should('contain', 'stats');
+      cy.go('back');
+      cy.go('back');
+      cy.location('pathname').should('not.contain', 'recoil');
+    });
   });
 
   it('changing locale works', () => {
