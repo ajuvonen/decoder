70de6b92c44bdbac5ee099313085d19288083afc
Anssi Juvonen <anssi.juvonen@gofore.com>
2023-02-13 16:07:18 +0200

    Properly use cypress chainable

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -9,22 +9,22 @@ describe('Navigation', () => {
     {buttonName: 'Context API', location: 'context'},
   ].forEach(({buttonName, location}) => {
     it(`with ${buttonName} works`, () => {
-      cy.getByTestId('version-selection').contains(buttonName).click();
-      cy.location('pathname').should('contain', `${location}/play`);
-      cy.getByTestId('information-modal').getByTestId('continue-button').click();
-      cy.getByTestId('stats-link').click();
-      cy.location('pathname').should('contain', 'stats');
-      cy.go('back');
-      cy.go('back');
-      cy.location('pathname').should('not.contain', 'recoil');
+      cy.getByTestId('version-selection').contains(buttonName).click()
+        .location('pathname').should('contain', `${location}/play`)
+        .getByTestId('information-modal').getByTestId('continue-button').click()
+        .getByTestId('stats-link').click()
+        .location('pathname').should('contain', 'stats')
+        .go('back')
+        .go('back')
+        .location('pathname').should('not.contain', 'recoil');
     });
   });
 
   it('changing locale works', () => {
-    cy.getByTestId('fi-locale-button').click();
-    cy.get('h1').should('contain', 'Tervetuloa');
-    cy.getByTestId('en-locale-button').click();
-    cy.get('h1').should('contain', 'Welcome');
+    cy.getByTestId('fi-locale-button').click()
+      .get('h1').should('contain', 'Tervetuloa')
+      .getByTestId('en-locale-button').click()
+      .get('h1').should('contain', 'Welcome');
   });
 });
 
