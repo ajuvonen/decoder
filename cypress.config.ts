0b315da6cdc97a42b229720de6a3929b9b9a2bf5
Anssi Juvonen <anssi.juvonen@gofore.com>
2023-02-15 11:44:05 +0200

    Add component test support

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from "cypress";
+import {defineConfig} from 'cypress';
 
 export default defineConfig({
+  video: false,
   e2e: {
     baseUrl: 'http://localhost:5173',
     viewportWidth: 1100,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
     },
   },
 });
