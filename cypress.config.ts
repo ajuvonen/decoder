1a9cbc27ba35aefdbd99c807b69832706c93320c
Anssi Juvonen <anssi.juvonen@gofore.com>
2023-02-16 10:55:29 +0200

    Add eslint to project

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,9 +5,6 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
     viewportWidth: 1100,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
   component: {
     devServer: {
