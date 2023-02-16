279fc1a76cb485d697aebd796dc99224b42ca61d
Anssi Juvonen <anssi.juvonen@gofore.com>
2023-02-16 10:55:29 +0200

    Add eslint to project

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    amd: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+  ignorePatterns: ['dist'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+};
