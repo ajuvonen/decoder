3620f15e2140b52de9e14dec0882aff92c4257ca
ajuvonen <ajuvonen@hotmail.com>
2023-10-24 15:44:06 +0300

    Remove language detection

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,6 +1,5 @@
 import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import fi from './fi.json';
 import en from './en.json';
 
@@ -13,7 +12,8 @@ export const resources = {
   },
 };
 
-i18n.use(initReactI18next).use(LanguageDetector).init({
+i18n.use(initReactI18next).init({
+  lng: 'en',
   returnNull: false,
   resources,
   fallbackLng: 'en',
